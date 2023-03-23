09251b66cb7b1f9a31ba80b68830d4a279c8d9ae
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-23 22:25:26 +0530

    success

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 // Write your JS code here
-import {Link} from 'react-router-dom'
+// Write your JS code here
 import {Component} from 'react'
 import Header from '../Header'
 import './index.css'
@@ -7,32 +7,28 @@ import './index.css'
 class Home extends Component {
   render() {
     return (
-      <Link to="/">
-        <div className="home-bg-container">
-          <Header />
-          <div className="home-body-container">
-            <div>
-              <h1 className="home-body-heading">
-                Clothes That Get You Noticed
-              </h1>
-              <p className="home-body-description">
-                Fashion is part of the daily air and it does not quite help that
-                changes all the time.
-              </p>
-              <button type="button" className="home-shop-now">
-                Shop Now
-              </button>
-            </div>
-            <div>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
-                alt="clothes that get you noticed"
-                className="home-body-image"
-              />
-            </div>
+      <div className="home-bg-container">
+        <Header />
+        <div className="home-body-container">
+          <div>
+            <h1 className="home-body-heading">Clothes That Get You Noticed</h1>
+            <p className="home-body-description">
+              Fashion is part of the daily air and it does not quite help that
+              changes all the time.
+            </p>
+            <button type="button" className="home-shop-now">
+              Shop Now
+            </button>
+          </div>
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+              alt="clothes that get you noticed"
+              className="home-body-image"
+            />
           </div>
         </div>
-      </Link>
+      </div>
     )
   }
 }
